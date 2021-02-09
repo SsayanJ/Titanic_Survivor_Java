5ec83eaf6093e92f021467c047c3984024b5ba7c
Jean-françois Benhaim <jean-francois.benhaim@epita.fr>
2021-02-09 13:15:42 +0100

    Submission

--- src/fr/epita/jfbenhaim/Tests/TestTDS3.java
+++ src/fr/epita/jfbenhaim/Tests/TestTDS3.java
@@ -11,11 +11,28 @@ public class TestTDS3 {
     public static void test {
         try {
             List<Passenger> passengerList= PassengerCSVDAO.readAllPredictions();
-            int TruePositive;
-            int TrueNegative;
-            int FalsePositive;
-            int FalseNegative;
-            
+            int TruePositive = 0;
+            int TrueNegative = 0;
+            int FalsePositive = 0;
+            int FalseNegative = 0;
+
+            for (Passenger passenger:passengerList){
+                if(passenger.getSurvived() && passenger.getPredicted()){
+                    TruePositive+=1;
+                } else if (!passenger.getPredicted() && !passenger.getSurvived()){
+                    TrueNegative+=1;
+                }else if (!passenger.getPredicted() && passenger.getSurvived()){
+                    FalseNegative+=1;
+                }else{
+                    FalsePositive+=1;
+                }
+            }
+
+            System.out.println("True Positives: "+TruePositive);
+            System.out.println("True Negatives: "+TruePositive);
+            System.out.println("False Positives: "+FalsePositive);
+            System.out.println("False Negatives: "+FalseNegative);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
